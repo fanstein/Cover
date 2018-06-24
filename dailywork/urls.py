91cb806c3405efb11875e4efdbb7f44ebb672237
fanstein <fanfly1607@163.com>
2018-06-24 22:39:33 +0800

    6.24

--- dailywork/urls.py
+++ dailywork/urls.py
@@ -8,11 +8,14 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'),name='home'),
     url(r'^notice/$', TemplateView.as_view(template_name='notice.html'),name='notice'),
     url(r'^json_tool/$', TemplateView.as_view(template_name='json_tool.html'), name='json_tool'),
+    url(r'^test/$', TemplateView.as_view(template_name='test.html'), name='test'),
     url(r'^perf/$', perf, name='perf'),
     url(r'^task/', get_task, name='task'),
     url(r'^result/', get_perf_result, name='get_perf_result'),
     url(r'^deploy',deploy,name='deploy'),
     url(r'^daily/', daily_task, name='daily_task'),
+
+
     url(r'^viewtest/$', ServerListView.as_view(), name='list'),
     url(r'^(?P<pk>[0-9]+)/$', ServerDetailView.as_view(), name='detail'),
 ]
